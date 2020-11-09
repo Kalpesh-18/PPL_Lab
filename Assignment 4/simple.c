6848e6b08e0d27476dec36b2f4612c409e0e1fdd
Kalpesh-18 <69343990+Kalpesh-18@users.noreply.github.com>
2020-11-09 21:51:57 +0530

    Add files via upload

---
+++ Assignment 4/simple.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	return 0; 
+}
+
+
+//(gdb) disassemble main
+/*Dump of assembler code for function main:
+   0x0000000000001129 <+0>:	endbr64 
+   0x000000000000112d <+4>:	push   %rbp
+   0x000000000000112e <+5>:	mov    %rsp,%rbp
+   0x0000000000001131 <+8>:	mov    $0x0,%eax
+   0x0000000000001136 <+13>:	pop    %rbp
+   0x0000000000001137 <+14>:	retq   
+End of assembler dump.
+*/
